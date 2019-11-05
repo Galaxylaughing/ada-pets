317af71c206e1f19e23f3e883260c767174bb830
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-05 10:14:10 -0800

    added index and show

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,13 +1,22 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
+    pets = Pet.all.as_json(only: [:id, :name, :age, :human])
+    render json: pets, status: :ok
   end
-
-
-
-  private
-
-    def pet_params
-      params.require(:pet).permit(:name, :age, :human)
+  
+  def show
+    pet = Pet.find_by(id: params[:id])
+    if pet
+      render json: pet, status: :ok
+    else
+      render json: {}, status: :not_found
     end
+  end
+  
+  
+  private
+  
+  def pet_params
+    params.require(:pet).permit(:name, :age, :human)
+  end
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,4 +1,77 @@
 require 'test_helper'
 
 describe PetsController do
+  describe "index" do
+    it "responds with JSON and success" do
+      get pets_path
+      
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+    
+    it "responds with an array of pet hashes" do
+      # Act
+      get pets_path
+      
+      # Get the body of the response
+      body = JSON.parse(response.body)
+      
+      # Assert
+      expect(body).must_be_instance_of Array
+      body.each do |pet|
+        expect(pet).must_be_instance_of Hash
+        expect(pet.keys.sort).must_equal ["age", "human", "id", "name"]
+      end
+    end
+    
+    it "will respond with an empty array when there are no pets" do
+      # Arrange
+      Pet.destroy_all
+      
+      # Act
+      get pets_path
+      body = JSON.parse(response.body)
+      
+      # Assert
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+    end
+  end
+  
+  describe "show" do
+    let(:pet) {
+      pets(:one)
+    }
+    
+    it "responds with JSON and success" do
+      get pet_path(pet.id)
+      
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+    
+    it "responds with a hash of pet data" do
+      get pet_path(pet.id)
+      
+      body = JSON.parse(response.body)
+      
+      expect(body).must_be_instance_of Hash
+      expect(body["id"]).must_equal pet.id
+      expect(body["age"]).must_equal pet.age
+      expect(body["human"]).must_equal pet.human
+      expect(body["name"]).must_equal pet.name
+    end
+    
+    it "will respond with empty hash and not_found if the pet does not exist" do
+      get pet_path(-1)
+      
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :not_found
+      
+      body = JSON.parse(response.body)
+      
+      expect(body).must_be_instance_of Hash
+      expect(body.empty?).must_equal true
+    end
+  end
 end
